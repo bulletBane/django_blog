604389b3275f6133dddb6520f1effff7806f5e6b
bulletBane <tolegena1@gmail.com>
2021-01-16 01:30:15 +0600

    load posts and page pagination

--- blog/models.py
+++ blog/models.py
@@ -1,32 +1,44 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
+
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
 
 
 class Post(models.Model):
+    """Define the post model"""
+    objects = models.Manager()
+    published = PublishedManager()
     STATUS_CHOICES = (
         ('draft', 'Draft'),
         ('published', 'Published'),
     )
-
     title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250,
+                            unique_for_date='publish')
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               related_name='blog_posts')
+    body = models.TextField(default='')
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True,)
+    updated = models.DateTimeField(auto_now=True,)
+    status = models.CharField(max_length=10,
+                              choices=STATUS_CHOICES, default='draft')
 
-    slug = models.SlugField(max_length=250, unique_for_date='publish')
+    class Meta:
+        ordering = ('-publish',)
 
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.publish.year,
+                                                 self.publish.month,
+                                                 self.publish.day,
+                                                 self.slug
+                                                 ])
 
-    published = models.DateTimeField(default=timezone.now)
-
-    created = models.DateTimeField(default=timezone.now)
-
-    updated = models.DateTimeField(default=timezone.now)
-
-    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
-
-
-class Meta:
-    ordering = ('-publsh',)
-
-
-def __str__(self):
-    return self.title
+    def __str__(self):
+        return self.title
